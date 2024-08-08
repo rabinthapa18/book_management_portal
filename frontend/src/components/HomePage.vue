e3453421d19f89596cce1ed46817b9b6b9ab9d4a
Rabinson <rabinson.thapa.18@gmail.com>
2024-08-08 23:49:37 +0900

    Added table actions to delete, edit and sort items

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,5 +1,46 @@
 <template>
     <h1>{{ heading }}</h1>
+    <div class="table-actions">
+        <v-row justify="start">
+            <!-- delete button -->
+            <v-col cols="12" md="2" class="mb-2">
+                <v-btn rounded="lg" block prepend-icon="mdi-delete" :disabled="selectedBooks.length === 0">
+                    Delete</v-btn>
+            </v-col>
+            <!-- edit button -->
+            <v-col cols="12" md="2" class="mb-2">
+                <v-btn rounded="lg" block prepend-icon="mdi-pencil" :disabled="selectedBooks.length !== 1">
+                    Edit</v-btn>
+            </v-col>
+            <!-- spacer -->
+            <v-col cols="12" md="5" class="mb-2">
+            </v-col>
+            <!-- sort dropdown -->
+            <v-col cols="12" md="3" class="mb-3">
+                <v-menu>
+                    <template v-slot:activator="{ props }">
+                        <v-btn v-bind="props" rounded="lg" block prepend-icon="mdi-sort" @click="sortDesc = !sortDesc">
+                            Sort by
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item @click="sortBooks('title', 'asc')">
+                            <v-list-item-title>Title Acending</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item @click="sortBooks('title', 'desc')">
+                            <v-list-item-title>Title Descending</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item @click="sortBooks('author', 'asc')">
+                            <v-list-item-title>Author Ascending</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item @click="sortBooks('author', 'desc')">
+                            <v-list-item-title>Author Descending</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </v-col>
+        </v-row>
+    </div>
 
     <div class="table-container">
         <table style="user-select: none">
@@ -56,7 +97,9 @@ export default {
             books: [],
             selectedBooks: [],
             selectAll: false,
-            isFetching: false
+            isFetching: false,
+            sortItem: null,
+            sortDesc: false,
         };
     },
     async mounted() {
@@ -125,6 +168,23 @@ export default {
                 ];
                 this.isFetching = false;
             }, 2000);
+        },
+
+        // sort books from API
+        sortBooks(sortItem, sortingMethod) {
+            // Simulating API for call
+            // TODO: Replace with actual API call
+            this.books.sort((a, b) => {
+                let itemA = a[sortItem].toString().toLowerCase();
+                let itemB = b[sortItem].toString().toLowerCase();
+
+                if (sortingMethod === 'asc') {
+                    return itemA < itemB ? -1 : itemA > itemB ? 1 : 0;
+                } else if (sortingMethod === 'desc') {
+                    return itemA > itemB ? -1 : itemA < itemB ? 1 : 0;
+                }
+                return 0;
+            });
         }
     }
 };
